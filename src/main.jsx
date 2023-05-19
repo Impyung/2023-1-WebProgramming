acfacf29293a48592ef788bc9895a3d2ce5d3c59
Pyung <pyung1591@gmail.com>
2023-05-19 12:17:44 +0900

    feat : 이태희 frame

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import Worldcup from './Worldcup.jsx';
 import './index.css';
+import Layout from './layout.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Worldcup />
+    <Layout />
   </React.StrictMode>
 );
